36fb70a370b725d2fe2bf972f169355f2451c725
GrayFlash <gk74533@gmail.com>
2023-03-19 03:07:34 +0530

    feat: update bio, add hackathon-exp

--- pages/index.js
+++ pages/index.js
@@ -21,18 +21,24 @@ export default function Home() {
             <h3>Hi, I am Gaurav Kumar</h3>
             <p>
               I am a final year undergrad in B.Tech, majoring in Electronics and Communication 
-              Engineering. Internet is huge, and so is my interest in technologies. I have been making
-              and breaking code for past 6 years (started with Java in high school).
-              For me the love to programming started with the thought of being able to write a logic
-              that was suitable to addressing the problems.
+              Engineering. I am currently an intern at <i>Chargebee</i>, where I work with 
+              <b> SRE</b> team on building solutions for reliable automation of cloud 
+              infrastructure. Cloud Engineering is relativly new for me since I have worked with 
+              ML and Backend-intensive projects so far and hence I am trying to do  better.
             </p>
           </div>
           <div className='column right'>
-          <img className="home-image" width="100%" src="/young-programmer-yellow-bg.jpeg" alt="Gaurav Kumar" />
+          <img className="home-image" width="100%" src="/dept_formal_gk.jpeg" alt="Gaurav Kumar" />
           </div>
         </div>
 
         <div className='row-text'>
+          <p>
+            Internet is huge, and so is my interest in technologies. I have been making
+            and breaking code for past 6 years (started with Java in high school).
+            For me the love to programming started with the thought of being able to write a logic
+            that was suitable to addressing the problems.
+          </p>
           <p>
             I have been exploring various tech-stacks for past 3 years, started with some simple web development,
             in my sophomore year continued with Machine Learning where I built most of my hackathon projects and
@@ -61,8 +67,18 @@ export default function Home() {
           <p>
             I was also a community leader for ACM NIT Surat, the programming student chapter and we conducted various 
             programming events throughout the year, and helped many of my juniors getting started with their journey of
-            computer science.
+            computer science. A key highlight around my tenure here was organising hackathon named Hack DotSlash.
+            This hackathon is quite close to my heart, and I have been rejected here as a participant, only to 
+            win it's next edition, then become it's lead organizer and mentor it in the 6th edition. And this 4 year 
+            association taught me a lot stuff about people, dev-community, devrels and organisations.
           </p>
+          <p>
+            Well, this should give some insight about me and the kind of things I build.
+            Feel free to checkout other section of my website as well, and thanks for visiting!
+          </p>
+          <br/>
+          <br/>
+          <br/>
         </div>
         <div className='row'>
           {/* TODO: add tech-stack */}
